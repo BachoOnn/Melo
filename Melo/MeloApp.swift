3d02a688778ef9c12faf72c33d435d5a6d37ff08
BachoOnn <oniani.bachoo@gmail.com>
2026-03-22 21:48:34 +0400

    Initial commit

---
+++ Melo/MeloApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeloApp.swift
+//  Melo
+//
+//  Created by Bacho on 22.03.26.
+//
+
+import SwiftUI
+
+@main
+struct MeloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
